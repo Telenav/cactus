67d785ba6ca2740628bae0f928ef28594d20e341
jonathan <jon@thanlocke.com>
2021-05-18 21:26:00 -0700

    👷 Download cactus-build to temporary folder, remove from assets folders

--- tools/library/cactus-build-common-profile.sh
+++ tools/library/cactus-build-common-profile.sh
@@ -8,6 +8,7 @@ system_variable CACTUS_TOOLS "$CACTUS_HOME/tools"
 system_variable CACTUS_JAVA_OPTIONS "-Xmx12g"
 
 system_variable LEXAKAI_VERSION 0.9.5-alpha-SNAPSHOT
+system_variable LEXAKAI_URL "https://s01.oss.sonatype.org/service/local/repositories/snapshots/content/com/telenav/lexakai/lexakai/0.9.5-alpha-SNAPSHOT/lexakai-0.9.5-alpha-20210519.035416-1.jar"
 
 append_path "$CACTUS_TOOLS/building"
 append_path "$CACTUS_TOOLS/library"
