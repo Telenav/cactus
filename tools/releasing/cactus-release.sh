a435c5f6c8c5fdab3402fbcab421d5afff887acc
jonathan <jon@thanlocke.com>
2022-03-14 18:36:18 -0600

    🔖 prepare for 1.4 release

--- tools/releasing/cactus-release.sh
+++ tools/releasing/cactus-release.sh
@@ -16,28 +16,47 @@ version=$1
 
 require_variable version "$help"
 
-bash cactus-release-start.sh "$version"
-bash cactus-release-update-version.sh "$version"
+if [ "$CACTUS_HOME" = "$version" ]; then
 
-bash cactus-build.sh
-bash cactus-build-documentation.sh
-bash cactus-build.sh deploy-local
+    bash cactus-build.sh
+    bash cactus-build-documentation.sh
+    bash cactus-build.sh deploy-local
 
-echo " "
-echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫ Release Created  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
-echo "┋"
-echo "┋  1. The branch release/$version has been created with git flow "
-echo "┋  2. Files containing version information such as pom.xml files have been updated to $version"
-echo "┋  3. The project and its documentation have been built"
-echo "┋  4. The project has been re-built from scratch and deployed to the local repository"
-echo "┋"
-echo "┋  Next Steps:"
-echo "┋"
-echo "┋  1. Check the release/$version branch carefully to make sure it's ready to go"
-echo "┋  2. Run cactus-release-finish.sh $version"
-echo "┋  3. Run cactus-build.sh deploy-ossrh"
-echo "┋  4. Sign into OSSRH and release to Maven Central"
-echo "┋"
-echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
-echo " "
+    echo " "
+    echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫ Release Created  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+    echo "┋"
+    echo "┋  1. The branch release/$version has been created with git flow "
+    echo "┋  2. Files containing version information such as pom.xml files have been updated to $version"
+    echo "┋  3. The project and its documentation have been built"
+    echo "┋  4. The project has been re-built from scratch and deployed to the local repository"
+    echo "┋"
+    echo "┋  Next Steps:"
+    echo "┋"
+    echo "┋  1. Check the release/$version branch carefully to make sure it's ready to go"
+    echo "┋  2. Run cactus-release-finish.sh $version"
+    echo "┋  3. Run cactus-build.sh deploy-ossrh"
+    echo "┋  4. Sign into OSSRH and release to Maven Central"
+    echo "┋"
+    echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+    echo " "
+
+else
+
+    bash cactus-release-start.sh "$version"
+    bash cactus-release-update-version.sh "$version"
+
+    echo " "
+    echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫ Release Created  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+    echo "┋"
+    echo "┋  1. The branch release/$version has been created with git flow "
+    echo "┋  2. Files containing version information such as pom.xml files have been updated to $version"
+    echo "┋"
+    echo "┋  Exit your terminal program entirely and restart it, then re-execute the command:"
+    echo "┋"
+    echo "┋  $0"
+    echo "┋"
+    echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+
+
+fi
 
