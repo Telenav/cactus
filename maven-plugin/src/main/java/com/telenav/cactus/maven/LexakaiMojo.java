24913a41f7af2fd15ec6f497e52ba246279eb264
Tim Boudreau <timb@telenav.com>
2022-06-07 14:49:21 -0400

    Initial fleshing out of lexakai mojo

--- maven-plugin/src/main/java/com/telenav/cactus/maven/LexakaiMojo.java
+++ maven-plugin/src/main/java/com/telenav/cactus/maven/LexakaiMojo.java
@@ -1,8 +1,12 @@
 package com.telenav.cactus.maven;
 
 import com.telenav.cactus.maven.log.BuildLog;
+import com.telenav.lexakai.Lexakai;
+import java.util.Arrays;
+import java.util.List;
 import static org.apache.maven.plugins.annotations.InstantiationStrategy.SINGLETON;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
@@ -16,10 +20,59 @@ import org.apache.maven.project.MavenProject;
         instantiationStrategy = SINGLETON,
         name = "lexakai", threadSafe = true)
 public class LexakaiMojo extends BaseMojo {
-    
+
+    @Parameter(property = "overwrite-resources", defaultValue = "true")
+    private boolean overwriteResources;
+
+    @Parameter(property = "update-readme", defaultValue = "true")
+    private boolean updateReadme;
+
+    @Parameter(property = "verbose", defaultValue = "true")
+    private boolean verbose;
+
+    @Parameter(property = "telenav.pretend", defaultValue = "false")
+    private boolean pretend;
+
+    @Parameter(property = "output-folder")
+    private String outputFolder;
+
     @Override
-    protected void performTasks(BuildLog log, MavenProject project) throws Exception
-    {
-        throw new UnsupportedOperationException("Run lexakai here.");
+    protected void performTasks(BuildLog log, MavenProject project) throws Exception {
+        String envVar = environmentVariableName(project);
+        String envValue = System.getenv(envVar);
+        log.info("Env var is " + envVar + " for " + project.getArtifactId() + " -> " + envValue);
+        String out = outputFolder;
+        if (out == null) {
+            out = envValue;
+        }
+        if (out == null) {
+            out = project.getBasedir().toPath().resolve("target").resolve("lexakai").toString();
+        }
+
+        List<String> args = Arrays.asList(
+                "-project-version=" + project.getVersion(),
+                "-update-readme=" + updateReadme,
+                "-overwrite-resources=" + overwriteResources,
+                "-output-folder=" + out,
+                project.getBasedir().toString()
+        );
+        if (verbose) {
+            log.info("Lexakai args:");
+            log.info("lexakai " + args);
+        }
+        if (!pretend) {
+            Lexakai.main(args.toArray(String[]::new));
+        }
+    }
+
+    private String environmentVariableName(MavenProject project) {
+        String gid = project.getGroupId();
+        int ix = gid.lastIndexOf('.');
+        String tail = gid.substring(ix + 1).toUpperCase();
+        int hyphen = tail.indexOf('-');
+        if (hyphen > 0) {
+            tail = tail.substring(0, hyphen);
+        }
+        return tail + "_ASSETS_HOME";
     }
 }
