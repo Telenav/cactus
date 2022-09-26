219df80a33546e7eed730101dd701931ba7a5192
Tim Boudreau <timb@telenav.com>
2022-09-26 15:47:11 -0400

    Fix #69 - CME in timeout process killer thread

--- cactus-process/src/main/java/com/telenav/cactus/process/KillQueue.java
+++ cactus-process/src/main/java/com/telenav/cactus/process/KillQueue.java
@@ -1,19 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// © 2011-2022 Telenav, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package com.telenav.cactus.process;
 
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.WeakHashMap;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 
 import static java.lang.Math.max;
-import static java.lang.Math.min;
 import static java.lang.System.currentTimeMillis;
-import static java.lang.Thread.sleep;
-import static java.util.Collections.synchronizedMap;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 /**
  * Kills processes that persist longer than a per-process timeout.
@@ -24,16 +39,15 @@ final class KillQueue
 {
 
     private static final KillQueue INSTANCE = new KillQueue();
-    private final Thread killThread = new Thread(this::killLoop,
-            "Process timeout killer");
-    private final Map<ProcessControl<?, ?>, Long> toKill = synchronizedMap(
-            new WeakHashMap<>());
+    private final DelayQueue<KillQueueEntry> toKill = new DelayQueue<>();
     private final AtomicBoolean started = new AtomicBoolean();
+    // So tests can verify processes get killed
+    static Consumer<ProcessControl<?, ?>> VERIFIER = ignored ->
+    {
+    };
 
     private KillQueue()
     {
-        killThread.setPriority(Thread.NORM_PRIORITY - 1);
-        killThread.setDaemon(true);
     }
 
     static void enqueue(Duration after, ProcessControl<?, ?> ctrl)
@@ -41,93 +55,105 @@ final class KillQueue
         INSTANCE._enqueue(after, ctrl);
     }
 
+    static boolean isStarted()
+    { // for tests
+        return INSTANCE.started.get();
+    }
+
     private void _enqueue(Duration after, ProcessControl<?, ?> ctrl)
     {
         long expires = currentTimeMillis() + after.toMillis();
-        toKill.put(ctrl, expires);
+        toKill.offer(new KillQueueEntry(ctrl, expires));
         if (started.compareAndSet(false, true))
         {
+            Thread killThread = new Thread(this::killLoop,
+                    "Process timeout killer");
+            killThread.setPriority(Thread.NORM_PRIORITY - 1);
+            killThread.setDaemon(true);
             killThread.start();
         }
-        else
-        {
-            killThread.interrupt();
-        }
-    }
-
-    private List<ProcessControl<?, ?>> removeTimedOut()
-    {
-        Set<ProcessControl<?, ?>> toRemove = new HashSet<>();
-        List<ProcessControl<?, ?>> timedOut = new ArrayList<>();
-        toKill.forEach((proc, expire) ->
-        {
-            if (!proc.state().isRunning())
-            {
-                toRemove.add(proc);
-            }
-            else
-            {
-                if (expire < currentTimeMillis())
-                {
-                    toRemove.add(proc);
-                    timedOut.add(proc);
-                }
-            }
-        });
-        for (ProcessControl ctrl : toRemove)
-        {
-            toKill.remove(ctrl);
-        }
-        return timedOut;
-    }
-
-    private long leastInterval()
-    {
-        long result = Long.MAX_VALUE;
-        for (Map.Entry<ProcessControl<?, ?>, Long> e : new HashSet<>(toKill.entrySet()))
-        {
-            long remaining = e.getValue() - currentTimeMillis();
-            result = min(result, remaining);
-            if (result < 0)
-            {
-                break;
-            }
-        }
-        return max(0, result);
     }
 
     private void killLoop()
     {
         // Note, while a DelayQueue would be appropriate here, that would not
         // let us wake up more frequently and prune exited processes
-        for (;;)
+        try
         {
-            try
+            for (;;)
             {
-                // Use a divisor so we can do some housekeeping
-                sleep(leastInterval() / 4);
+                try
+                {
+                    toKill.take().kill();
+                }
+                catch (Exception | Error ex)
+                {
+                    if (!(ex instanceof InterruptedException))
+                    {
+                        // Hmm, is this still ever used with the removal of
+                        // Thread.stop()?
+                        if (ex instanceof ThreadDeath)
+                        {
+                            throw ((ThreadDeath) ex);
+                        }
+                        ex.printStackTrace();;
+                    }
+                }
             }
-            catch (InterruptedException ex)
-            {
-                // ok, that's how we wake up, so we recompute the sleep interval
-            }
-            removeTimedOut().forEach(this::killOne);
+        }
+        finally
+        {
+            // If we did exit for some unusual reason, the next call
+            // to enqueue will start a new kill thread.
+            started.set(false);
         }
     }
 
-    private void killOne(ProcessControl<?, ?> ctrl)
+    private final class KillQueueEntry implements Delayed
     {
-        if (ctrl.state().isRunning())
+        private final Reference<ProcessControl<?, ?>> processRef;
+        private final long expiresAt;
+
+        public KillQueueEntry(
+                ProcessControl<?, ?> process, long expiresAt)
         {
-            try
+            this.processRef = new WeakReference<>(process);
+            this.expiresAt = expiresAt;
+        }
+
+        boolean isDead()
+        {
+            return processRef.get() == null;
+        }
+
+        boolean kill()
+        {
+            ProcessControl<?, ?> process = processRef.get();
+            if (process != null)
             {
-                ctrl.kill();
-                System.err.println("Killed " + ctrl);
+                VERIFIER.accept(process);
+                return process.kill();
             }
-            catch (Exception | Error ex)
+            return false;
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit)
+        {
+            if (isDead())
             {
-                ex.printStackTrace(System.err);
+                return 0;
             }
+            return unit.convert(max(0, expiresAt - currentTimeMillis()),
+                    MILLISECONDS);
+        }
+
+        @Override
+        public int compareTo(Delayed o)
+        {
+            // Why oh why does Delayed not add a default method for this?
+            return Long
+                    .compare(getDelay(MILLISECONDS), o.getDelay(MILLISECONDS));
         }
     }
 
