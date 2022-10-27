15390d7109ac34ae2aecec11c33d023f77c7c4e7
jonathanl-telenav <jonathanl@telenav.com>
2022-10-27 08:20:08 -0600

    🔨 fix script syntax error

--- release.sh
+++ release.sh
@@ -7,6 +7,6 @@ echo "Instructions to complete the release :"
 echo " "
 echo "  1. Sign into OSSRH (https://s01.oss.sonatype.org/)"
 echo "  2. Close the staging repository and release it"
-echo "  3. Wait for the release to be copied to Maven Central (https://repo1.maven.org/maven2/com/telenav/cactus/")
+echo "  3. Wait for the release to be copied to Maven Central (https://repo1.maven.org/maven2/com/telenav/cactus/"
 echo "  4. Run cbump"
 echo " "
